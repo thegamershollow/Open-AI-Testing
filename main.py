19496d8736d43e58243157a163f70b1c26cbf0a2
thegamershollow <thegamershollow@outlook.com>
2023-03-20 17:25:04 +0000

    added api call and chatgpt config

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 import openai
-api = (api.txt)
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
+api_call = open('api.txt')
+api = api_call.read()
+openai.api_key = api
+
+question = input("Question for ChatGPT:")
+openai.ChatCompletion.create(
+model="gpt-3.5-turbo",
+messages=[
+    {"role": "user", "content": question}
+    ]
+)
 
 
