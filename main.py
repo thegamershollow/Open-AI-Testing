9389b9092c5b8047fb5ce0582e518b0349d6871f
thegamershollow <thegamershollow@outlook.com>
2023-03-20 15:56:42 +0000

    added api key to file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 import openai
 openai.organization = "org-C69lk72lYbzrRrBifeRKi6l4"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 openai.Model.list()
